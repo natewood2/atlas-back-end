097fe3c75682214978084f7164101a3ec66546a3
root <nathanwood.sfg@gmail.com>
2024-03-14 17:55:56 +0000

    Adding 1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" File for pulling from APIs. """
+import requests
+import sys
+import csv
+
+
+def TODO_list_csv(employee_id):
+
+    api_url = "https://jsonplaceholder.typicode.com"
+    users_url = f"{api_url}/users/{employee_id}"
+    todos_url = f"{api_url}/todos"
+
+    user_data = requests.get(users_url).json()
+    username = user_data.get('username')
+
+    todos = requests.get(todos_url, params={'userId': employee_id}).json()
+    csv_file_name = f"{employee_id}.csv"
+    with open(csv_file_name, mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for todo in todos:
+            writer.writerow([employee_id, username, todo['completed'], todo['title']])
+
+    print("Well that worked fine...")
+
+if __name__ == "__main__":
+
+    TODO_list_csv(int(sys.argv[1]))
