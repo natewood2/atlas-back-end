36915c68ad206eaa9aa216f85d941140915fe243
natewood2 <nathanwood.sfg@gmail.com>
2024-03-14 11:21:23 -0500

    Adding 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,13 +13,15 @@ def TODO_list_progress(employee_id):
         return
     employee_name = get_employee_name.json().get('name')
 
-    todos_total = requests.get(f"{api_url}/todos", params={'userID': employee_id})
+    todos_total = requests.get(f"{api_url}/todos", 
+                               params={'userID': employee_id})
     if todos_total.status_code != 200:
         print("Failed to get that TODO list bud.")
         return
     todos = todos_total.json()
     finished_tasks = [todos for todo in todos if todo['completed']]
 
-    print(f"Employee {employee_name} is done with tasks({len(finished_tasks)}/{len(todos)}):")
+    print(f"Employee {employee_name} is 
+          done with tasks({len(finished_tasks)}/{len(todos)}):")
     for task in finished_tasks:
         print(f"\t {task['title']}")
